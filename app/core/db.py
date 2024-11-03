0e8f3ecc30a1ace94ea81e5a3f7cd27d806d72d1
Victor Ryabov <victorwillhard@gmail.com>
2024-11-03 14:37:19 +0300

    Refactor Base class, add async engine and session setup

--- app/core/db.py
+++ app/core/db.py
@@ -1,4 +1,20 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, declared_attr, sessionmaker
+
+from app.core.config import settings
 
 
-Base = declarative_base()
+class PreBase:
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    id = Column(Integer, primary_key=True)
+
+
+Base = declarative_base(cls=PreBase)
+
+engine = create_async_engine(settings.database_url)
+
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
