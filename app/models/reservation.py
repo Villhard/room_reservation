c571426f6775e61d1fbce12920f53d298f9c94b1
Victor Ryabov <victorwillhard@gmail.com>
2024-11-05 18:27:01 +0300

    Add __repr__ method to Reservation class for better instance representation

--- app/models/reservation.py
+++ app/models/reservation.py
@@ -7,3 +7,6 @@ class Reservation(Base):
     from_reserve = Column(DateTime)
     to_reserve = Column(DateTime)
     meetingroom_id = Column(Integer, ForeignKey("meetingroom.id"))
+
+    def __repr__(self):
+        return f"Уже забронировано с {self.from_reserve} до {self.to_reserve}"
