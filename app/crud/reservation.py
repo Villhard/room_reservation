27ad81db301b6e89fbc3a7ef041f4d2baeb255f5
Victor Ryabov <victorwillhard@gmail.com>
2024-11-05 17:17:16 +0300

    Add meetingroom_id filter and query logic to get overlapping reservations

--- app/crud/reservation.py
+++ app/crud/reservation.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, and_
 
 from app.crud.base import CRUDBase
 from app.models.reservation import Reservation
@@ -14,10 +15,21 @@ class CRUDReservation(CRUDBase):
         self,
         from_reserve: datetime,
         to_reserve: datetime,
+        meetingroom_id: int,
         session: AsyncSession,
     ):
-        ...
-        return []
+        reservations = await session.execute(
+            select(Reservation).where(
+                Reservation.meetingroom_id == meetingroom_id,
+                and_(
+                    from_reserve <= Reservation.to_reserve,
+                    to_reserve >= Reservation.from_reserve,
+                ),
+            )
+        )
+        reservations = reservations.scalars().all()
+
+        return reservations
 
 
 reservation_crud = CRUDReservation()
