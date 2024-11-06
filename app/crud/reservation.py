132cd443d405d502d10c07840d6df9a5d18be9a7
Victor Ryabov <victorwillhard@gmail.com>
2024-11-06 03:56:53 +0300

    Add async method to fetch future reservations for the room.

--- app/crud/reservation.py
+++ app/crud/reservation.py
@@ -37,5 +37,20 @@ class CRUDReservation(CRUDBase):
 
         return reservations
 
+    async def get_future_reservations_for_room(
+        self,
+        room_id: int,
+        session: AsyncSession,
+    ):
+        reservations = await session.execute(
+            select(Reservation).where(
+                Reservation.meetingroom_id == room_id,
+                Reservation.to_reserve > datetime.now(),
+            )
+        )
+        reservations = reservations.scalars().all()
+
+        return reservations
+
 
 reservation_crud = CRUDReservation()
