fbce3e79a85763a705aa5dbd20be1e7e40170cef
Victor Ryabov <victorwillhard@gmail.com>
2024-11-06 03:19:11 +0300

    Add optional reservation_id filter and refactor query in get_reservations_at_same_time

--- app/crud/reservation.py
+++ app/crud/reservation.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from datetime import datetime
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -13,20 +14,25 @@ class CRUDReservation(CRUDBase):
 
     async def get_reservations_at_the_same_time(
         self,
+        *,
         from_reserve: datetime,
         to_reserve: datetime,
         meetingroom_id: int,
+        reservation_id: Optional[int] = None,
         session: AsyncSession,
     ):
-        reservations = await session.execute(
-            select(Reservation).where(
-                Reservation.meetingroom_id == meetingroom_id,
-                and_(
-                    from_reserve <= Reservation.to_reserve,
-                    to_reserve >= Reservation.from_reserve,
-                ),
-            )
+        select_stmt = select(Reservation).where(
+            Reservation.meetingroom_id == meetingroom_id,
+            and_(
+                from_reserve <= Reservation.to_reserve,
+                to_reserve >= Reservation.from_reserve,
+            ),
         )
+
+        if reservation_id is not None:
+            select_stmt = select_stmt.where(Reservation.id != reservation_id)
+
+        reservations = await session.execute(select_stmt)
         reservations = reservations.scalars().all()
 
         return reservations
