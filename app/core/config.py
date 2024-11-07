67e906e127a99499d47d9e694af0830cdaeea116
Victor Ryabov <victorwillhard@gmail.com>
2024-11-07 03:37:00 +0300

    Set default values for app_title and secret; add user authentication setup.

--- app/core/config.py
+++ app/core/config.py
@@ -2,8 +2,9 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_title: str
+    app_title: str = "Бронирование переговорок"
     database_url: str
+    secret: str = "secret"
 
     class Config:
         env_file = ".env"
