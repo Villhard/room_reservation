f9203d8b2114059946f9be08d5f8f66d96c0e4a7
Victor Ryabov <victorwillhard@gmail.com>
2024-11-07 03:55:28 +0300

    Add user router to API and main router configuration

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -1,4 +1,5 @@
 from .meeting_room import router as meeting_room_router
 from .reservation import router as reservation_router
+from .user import router as user_router
 
-__all__ = ["meeting_room_router", "reservation_router"]
+__all__ = ["meeting_room_router", "reservation_router", "user_router"]
